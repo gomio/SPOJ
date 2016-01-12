18db68ba987d7517bd843c1004221e037ed0e626
gomio <gomio@wanadoo.es>
2016-01-12 13:17:58 +0100

    Actualizacion

--- 00001-TEST/lua/test.lua
+++
@@ -1,4 +0,0 @@
-for line in io.lines() do
-   if (line == "42") then break end
-   print(line)
-end
--- 06694-BOMARBLE/bomarble_MAKE.bat
+++
@@ -1,9 +0,0 @@
-@pskill -t >nul 2>nul mingw32-make.exe 
-@pskill -t >nul 2>nul make.exe 
-@pskill -t >nul 2>nul gcc.exe 
-@pskill -t >nul 2>nul g++.exe 
-@pskill -t >nul 2>nul ld.exe 
-@pskill -t >nul 2>nul collect2.exe 
-mingw32-make.exe -f bomarble_MAKE.mak 
-@echo. 
-@if '%NOM%'=='' pause 
